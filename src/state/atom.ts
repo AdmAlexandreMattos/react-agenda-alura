9dc35649782d5180bc63c5bffa1d4f786a03abcc
AdmAlexandreMattos <admalexandremattos@gmail.com>
2025-07-01 18:44:30 -0300

    Valores padrões para o átomo
    Concluindo a lógica da lista de eventos para exibirem após o carregamento.

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -1,25 +1,11 @@
 import { atom } from "recoil";
 import { IEvento } from "../interfaces/IEvento";
 import { IFiltroEventos } from "../interfaces/IFiltroEventos";
+import { eventosAsync } from "./selectors";
 
 export const listaEventosState = atom<IEvento[]>({
   key: "listaEventosState",
-  default: [
-    {
-      descricao: "Estudar React",
-      inicio: new Date("2025-06-25T12:00"),
-      fim: new Date("2025-06-25T14:00"),
-      completo: false,
-      id: 1642342747,
-    },
-    {
-      descricao: "Estudar Recoil",
-      inicio: new Date("2025-06-25T09:00"),
-      fim: new Date("2025-06-25T11:00"),
-      completo: false,
-      id: 1642342959,
-    },
-  ],
+  default: eventosAsync,
 });
 
 export const filtroEventos = atom<IFiltroEventos>({
