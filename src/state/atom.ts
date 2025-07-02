56581a269d4bc331bc985f8a707f2219e117b1a2
AdmAlexandreMattos <admalexandremattos@gmail.com>
2025-07-02 13:05:37 -0300

    Criando branch para publicaar na vercel
    Alterando a lógica de carregar a array de objeto dos eventos já cadastrados.

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -5,7 +5,37 @@ import { eventosAsync } from "./selectors";
 
 export const listaEventosState = atom<IEvento[]>({
   key: "listaEventosState",
-  default: eventosAsync,
+  default: [
+    {
+      descricao: "Estudar React",
+      inicio: new Date("2025-07-01T09:00"),
+      fim: new Date("2025-07-01T11:00"),
+      completo: false,
+      id: 164234274711,
+    },
+    {
+      descricao: "Estudar Recoil",
+      inicio: new Date("2025-07-03T12:00"),
+      fim: new Date("2025-07-03T14:00"),
+      completo: false,
+      id: 164234295912,
+    },
+    {
+      descricao: "Estudar JavaScript",
+      inicio: new Date("2025-07-10T12:00"),
+      fim: new Date("2025-07-10T14:00"),
+      completo: false,
+      id: 164234295913,
+    },
+    {
+      descricao: "Estudar TypeScript",
+      inicio: new Date("2025-07-11T12:00"),
+      fim: new Date("2025-07-11T14:00"),
+      completo: false,
+      id: 164234295914,
+    },
+  ],
+  // default: eventosAsync,
 });
 
 export const filtroEventos = atom<IFiltroEventos>({
